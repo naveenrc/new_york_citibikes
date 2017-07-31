57c90e15004d16f17847767248db51bd705e354d
Naveen <naveenrch@yahoo.com>
2017-07-31 19:19:31 -0400

    Stations info obtained

---
+++ download/station_info.py
@@ -0,0 +1,15 @@
+import requests
+import pandas as pd
+
+# This url provides info about stations
+response = requests.get('https://gbfs.citibikenyc.com/gbfs/en/station_information.json')
+content = response.json()
+# get stations info from response
+stations = content['data']['stations']
+# convert it to dataframe
+data = pd.DataFrame(stations)
+data = data.drop(["capacity", "rental_methods", "short_name", "eightd_has_key_dispenser"], axis=1)
+data = data.set_index('station_id')
+
+# write to file
+data.to_csv('C:/Users/Naveen/Downloads/Springboard/GitHub/new_york_citibikes/data/station_info.csv')
